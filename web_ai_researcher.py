ca7825fa7052e26b9ff8191ddce590baa1144d84
MaxData <max@workhere.org>
2025-09-08 20:59:57 -0400

    c

--- web_ai_researcher.py
+++ web_ai_researcher.py
@@ -18,9 +18,11 @@ import base64
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
 # If you want to use proxy, please uncomment the following lines
-os.environ['https_proxy'] = 'http://100.68.161.73:3128'
-os.environ['http_proxy'] = 'http://100.68.161.73:3128'
-os.environ['no_proxy'] = 'localhost,127.0.0.1,0.0.0.0'
+# Optional proxy settings: enable by setting USE_PROXY=1 in your environment
+if os.getenv("USE_PROXY") == "1":
+    os.environ['https_proxy'] = os.getenv('HTTPS_PROXY', 'http://100.68.161.73:3128')
+    os.environ['http_proxy'] = os.getenv('HTTP_PROXY', 'http://100.68.161.73:3128')
+    os.environ['no_proxy'] = os.getenv('NO_PROXY', 'localhost,127.0.0.1,0.0.0.0')
 
 def setup_path():
     # logs_dir = os.path.join("casestudy_results", f'agent_{container_name}', 'logs')
